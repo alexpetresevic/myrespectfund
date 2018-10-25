7434979cfe033a43be670f2bbde6c8af54194774
Robert Topolovec <robert.topolovec@extraordinary.rs>
2018-10-25 16:43:09 +0200

    minor fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -181,11 +181,6 @@
                                                                     href="{{ route('funeral-home.create') }}">submit my
                                                                 business</a></li>
 
-                                                               href="{{ route('campaign.create') }}">create your fundraiser</a>
-                                                        </li>
-                                                        <li><a class="dropdown-item"
-                                                               href="{{ route('funeral-home.create') }}">submit my business</a></li>
-
                                                         {{--<li class="signin__mobile"><a class="dropdown-item"--}}
                                                         {{--href="{{ route('login') }}">sign up</a>--}}
                                                         {{--</li>--}}
@@ -221,11 +216,7 @@
                                                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">logout</a>
                                                         </li>
                                                     @endif
-                                                    <li><a class="dropdown-item"
 
-                                                    </li>
-
-                                                    </li>
                                                     <li><a class="dropdown-item"
                                                            href="{{ route('blog.index') }}">blog</a>
                                                     </li>                                                    
@@ -251,10 +242,6 @@
                                                     <li><a class="dropdown-item"
                                                            href="{{ route('page.contact') }}">contact us</a>
                                                     </li>
-
-                                                           href="{{ route('page.contact') }}">contact us</a>
-                                                    </li>                                                                       
-
                                                     <!-- <li><a class="dropdown-item" href="/find-funeral-home">Find a
                                                             funeral
                                                             home</a>
